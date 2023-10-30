7872726aadea7bc69616e2752ce8adca23ba6192
douglasmsi <douglas.marra@mercadolivre.com>
2023-10-30 19:48:44 -0300

    nova tentativa

--- AWS_rds.tf
+++ AWS_rds.tf
@@ -12,59 +12,32 @@ resource "aws_security_group" "sec_grp_rds" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = var.tags
 }
 
-resource "aws_security_group_rule" "allow-workers-nodes-communications" {
-  description              = "Allow worker nodes to communicate with database"
-  from_port                = 5432
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.sec_grp_rds.id
-  source_security_group_id = module.cluster.cluster_security_group_id
-  to_port                  = 5432
-  type                     = "ingress"
-}
-
-#####
-# DB
-#####
-module "db" {
-  version = "~> 2.0"
-  source  = "terraform-aws-modules/rds/aws"
-
-  identifier = module.cluster.cluster_id
-
-  engine              = var.rds_engine
-  engine_version      = var.rds_engine_version
-  instance_class      = var.rds_instance_class
-  allocated_storage   = var.rds_allocated_storage
-  storage_encrypted   = false
+resource "aws_db_instance" "tech-challenge-database" {
+  allocated_storage   = 20
+  storage_type        = "gp2"
+  identifier          = "tech-challenge"
+  engine              = "postgres"
+  engine_version      = "14"
+  instance_class      = "db.t3.micro"
+  db_name             = "tech-challenge-database"
+  username            = "fastfooduser"
+  password            = "fastfoodpass"
+  port                = 5432
   publicly_accessible = true
-
-  name = var.rds_name
-
-  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
-  # user cannot be used as it is a reserved word used by the engine"
-  username = var.rds_username
-
-  password = var.rds_password
-  port     = var.rds_port
-
-  vpc_security_group_ids = [aws_security_group.sec_grp_rds.id]
-
-  maintenance_window = "Mon:00:00-Mon:03:00"
-  backup_window      = "03:00-06:00"
-
-  # disable backups to create DB faster
-  backup_retention_period = 0
-
-  tags = var.tags
-
-  # DB subnet group
+  skip_final_snapshot = true
   subnet_ids = module.vpc.public_subnets
-
-  # DB parameter group
-  family = var.rds_parameter_group_family
+  vpc_security_group_ids = [aws_security_group.sec_grp_rds.id]
+  tags = {
+    Name = "tech-challenge-rds"
+  }
 }
