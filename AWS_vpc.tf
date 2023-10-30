eead880c08ca4846322bb0650024dd54254f9be2
douglasmsi <douglas.marra@mercadolivre.com>
2023-10-30 20:42:21 -0300

    nova tentativa

--- AWS_vpc.tf
+++ AWS_vpc.tf
@@ -10,10 +10,10 @@ module "vpc" {
 
   azs            = var.azs
 
-  private_subnets =var.private_subnets
-  public_subnets = var.public_subnets
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
 
-  cidr = "10.0.0.0/20"
+  cidr = "10.0.0.0/16"
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
