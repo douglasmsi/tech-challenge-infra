66ee755e6eab15777105d99efa9d3bf9c2a2e153
douglasmsi <douglas.marra@mercadolivre.com>
2023-10-30 19:55:31 -0300

    nova tentativa

--- outputs.tf
+++ outputs.tf
@@ -80,78 +80,10 @@ output "public_route_table_ids" {
   value = module.vpc.public_route_table_ids
 }
 
-##########################################################################################
-# AWS RDS Output
-output "db_instance_address" {
-  value = module.db.this_db_instance_address
-}
 
-output "db_instance_arn" {
-  value = module.db.this_db_instance_arn
-}
-
-output "db_instance_availability_zone" {
-  value = module.db.this_db_instance_availability_zone
-}
-
-output "db_instance_endpoint" {
-  value = module.db.this_db_instance_endpoint
-}
-
-output "db_instance_hosted_zone_id" {
-  value = module.db.this_db_instance_hosted_zone_id
-}
-
-output "db_instance_id" {
-  value = module.db.this_db_instance_id
-}
-
-output "db_instance_name" {
-  value = module.db.this_db_instance_name
-}
-
-output "db_instance_port" {
-  value = module.db.this_db_instance_port
-}
-
-output "db_instance_resource_id" {
-  value = module.db.this_db_instance_resource_id
-}
-
-output "db_instance_status" {
-  value = module.db.this_db_instance_status
-}
-
-output "db_instance_username" {
-  value = module.db.this_db_instance_username
-}
-
-output "db_option_group_arn" {
-  value = module.db.this_db_option_group_arn
-}
-
-output "db_option_group_id" {
-  value = module.db.this_db_option_group_id
-}
-
-output "db_parameter_group_arn" {
-  value = module.db.this_db_parameter_group_arn
-}
-
-output "db_parameter_group_id" {
-  value = module.db.this_db_parameter_group_id
-}
-
-output "db_subnet_group_arn" {
-  value = module.db.this_db_subnet_group_arn
-}
-
-output "db_subnet_group_id" {
-  value = module.db.this_db_subnet_group_id
-}
 
 output "jdbc_url" {
-  value = "jdbc:postgresql://${module.db.this_db_instance_endpoint}/${module.db.this_db_instance_name}"
+  value = "jdbc:postgresql://${aws_db_instance.tech-challenge-database.endpoint}/${aws_db_instance.tech-challenge-database.db_name}"
 }
 
 output "cluster_endpoint" {
