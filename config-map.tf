1315fc89ed8d02f37dc97d1cedfd80383f98159a
douglasmsi <douglas.marra@mercadolivre.com>
2023-10-31 09:07:50 -0300

    changes

--- config-map.tf
+++ config-map.tf
@@ -1,12 +1,12 @@
 resource "kubernetes_config_map" "tech-challenge-config-map" {
   metadata {
     name = "postgres-config-map"
-    namespace = kubernetes_namespace.tech-challenge.metadata.0.name
+    namespace = kubernetes_namespace.tech-challenge-namespaces.metadata.0.name
   }
 
   data = {
     postgres-server        = "tech-challenge-app-postgres"
-    postgres-database-name = "tech-challenge-app-database"
+    postgres-database-name = "techchallengedb"
     postgres-user-username = "tech-challenge-user"
     postgres-user-password = "tech-challenge-password"
   }
