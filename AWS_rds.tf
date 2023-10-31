40e397120796d84d0c8f0df8d504c9fa35f55877
douglasmsi <douglas.marra@mercadolivre.com>
2023-10-31 08:36:12 -0300

    changes

--- AWS_rds.tf
+++ AWS_rds.tf
@@ -3,7 +3,7 @@
 ##############################################################
 
 resource "aws_security_group" "sec_grp_rds" {
-  name_prefix = "${var.name}-"
+  name_prefix = "sg-"
   vpc_id      = module.vpc.vpc_id
 
   egress {
