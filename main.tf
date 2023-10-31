587f72bde7adce8f480f98112f830b4434e64827
douglasmsi <douglas.marra@mercadolivre.com>
2023-10-31 11:42:00 -0300

    secrets

--- main.tf
+++ main.tf
@@ -158,3 +158,23 @@ resource "kubernetes_namespace" "tech-challenge-namespace" {
         name = "tech-challenge-namespace"
     }
 }
+
+resource "kubernetes_secret" "tech-challenge-secret" {
+  metadata {
+    name = "ecr-registry"
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "${var.registry_server}" = {
+          "username" = var.registry_username
+          "password" = var.registry_password
+          "auth"     = base64encode("${var.registry_username}:${var.registry_password}")
+        }
+      }
+    })
+  }
+}
