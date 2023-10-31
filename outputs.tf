83501d06ff0ec39533a38bef9c17a5ef5b90a8c0
douglasmsi <douglas.marra@mercadolivre.com>
2023-10-30 22:29:23 -0300

    nova tentativa

--- outputs.tf
+++ outputs.tf
@@ -4,26 +4,26 @@
 
 
 output "cluster_certificate_authority_data" {
-  value = module.cluster.cluster_certificate_authority_data
+  value = module.eks.cluster_certificate_authority_data
 }
 
 
 
 output "cluster_iam_role_arn" {
-  value = module.cluster.cluster_iam_role_arn
+  value = module.eks.cluster_iam_role_arn
 }
 
 output "cluster_iam_role_name" {
-  value = module.cluster.cluster_iam_role_name
+  value = module.eks.cluster_iam_role_name
 }
 
 output "cluster_id" {
-  value = module.cluster.cluster_id
+  value = module.eks.cluster_id
 }
 
 
 output "cluster_version" {
-  value = module.cluster.cluster_version
+  value = module.eks.cluster_version
 }
 
 
@@ -88,12 +88,12 @@ output "jdbc_url" {
 
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane"
-  value       = module.cluster.cluster_endpoint
+  value       = module.eks.cluster_endpoint
 }
 
 output "cluster_security_group_id" {
   description = "Security group ids attached to the cluster control plane"
-  value       = module.cluster.cluster_security_group_id
+  value       = module.eks.cluster_security_group_id
 }
 
 output "region" {
@@ -103,5 +103,5 @@ output "region" {
 
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
-  value       = module.cluster.cluster_name
+  value       = module.eks.cluster_name
 }
