53a0b7f81a2421ac4adaaaee749a5dc0fddcaa7a
douglasmsi <douglas.marra@mercadolivre.com>
2023-10-30 22:57:54 -0300

    nova tentativa

--- AWS_eks.tf
+++ AWS_eks.tf
@@ -34,34 +34,6 @@ resource "aws_eks_addon" "ebs-csi" {
 }
 
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-
-resource "kubernetes_namespace" "tech-challenge-namespace" {
-  metadata {
-    annotations = {
-      name = "tech-challenge-annotation"
-    }
-
-    labels = {
-      tech-challenge = "tech-challenge"
-    }
-
-    name = "tech-challenge-namespace"
-  }
-}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -101,3 +73,33 @@ module "eks" {
     }
   }
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+
+resource "kubernetes_namespace" "tech-challenge-namespace" {
+  metadata {
+    annotations = {
+      name = "tech-challenge-annotation"
+    }
+
+    labels = {
+      tech-challenge = "tech-challenge"
+    }
+
+    name = "tech-challenge-namespace"
+  }
+}
+
