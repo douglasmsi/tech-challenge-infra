e6f23cf73159965dd7b02eb20dbd1a5f653ca4ee
douglasmsi <douglas.marra@mercadolivre.com>
2023-10-31 09:24:18 -0300

    changes

--- main.tf
+++ main.tf
@@ -142,16 +142,3 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.tech-challenge.token
 }
-
-
-
-resource "kubernetes_namespace" "tech-challenge-namespaces" {
-  metadata {
-    name = "tech-challenge-namespaces"
-  }
-}
-
-
-
-
-
