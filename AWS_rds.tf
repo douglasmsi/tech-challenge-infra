70267b73c4277e725b05989ce9b64f5f402b5c9f
douglasmsi <douglas.marra@mercadolivre.com>
2023-10-31 08:42:48 -0300

    changes

--- AWS_rds.tf
+++ AWS_rds.tf
@@ -16,9 +16,8 @@ resource "aws_security_group" "sec_grp_rds" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    security_groups = [
-      aws_db_subnet_group.rds_subnet_group.name,
-    ]
+    security_groups = [aws_security_group.sec_grp_rds.id]
+
   }
 
   tags = {
