f39a81fa474820937eb8d93a63a9e4048df23646
douglasmsi <douglas.marra@mercadolivre.com>
2023-11-01 00:26:56 -0300

    update

--- main.tf
+++ main.tf
@@ -129,35 +129,6 @@ data "aws_eks_cluster_auth" "tech-challenge" {
 }
 
 
-resource "kubernetes_namespace" "tech-challenge-namespace" {
-  metadata {
-    name = "tech-challenge-namespace"
-  }
-
-}
-
-resource "kubernetes_secret" "tech-challenge-secret" {
-  metadata {
-    name = "ecr-registry"
-  }
-
-  type = "kubernetes.io/dockerconfigjson"
-
-  data = {
-    ".dockerconfigjson" = jsonencode({
-      auths = {
-        (var.registry_server) = {
-          "username" = var.registry_username
-          "password" = var.registry_password
-          "auth"     = base64encode("${var.registry_username}:${var.registry_password}")
-        }
-      }
-    })
-  }
-
-}
-
-
 
 
 
