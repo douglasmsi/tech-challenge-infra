c983ece69b6a63ef779bd7652d2888f0c9e3ad72
douglasmsi <douglas.marra@mercadolivre.com>
2023-11-01 22:15:36 -0300

    Versao final

--- main.tf
+++ main.tf
@@ -1,12 +1,9 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
 
 provider "aws" {
   region = var.region
 }
 
-# Filter out local zones, which are not currently supported
-# with managed node groups
+
 data "aws_availability_zones" "available" {
   filter {
     name   = "opt-in-status"
@@ -34,9 +31,8 @@ module "eks" {
         from_port   = 80
         to_port     = 80
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]  # Isso permite o tráfego de qualquer lugar
+        cidr_blocks = ["0.0.0.0/0"]
       },
-      # ... outras regras se necessário ...
     ]
 
   }
@@ -70,36 +66,7 @@ module "eks" {
 
 }
 
-/*
-# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
-
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "4.7.0"
-
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
-
-resource "aws_eks_addon" "ebs-csi" {
-  cluster_name             = module.eks.cluster_name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.24.0-eksbuild.1"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-  tags = {
-    "eks_addon" = "ebs-csi"
-    "terraform" = "true"
-  }
-}
-*/
 locals {
-  //cluster_name = "tech-challenge-eks-${random_string.suffix.result}"
   cluster_name = var.cluster_name
 }
 
@@ -138,6 +105,7 @@ module "vpc" {
     team = "tech-challenge"
   }
 }
+
 
 
 data "aws_eks_cluster_auth" "tech-challenge" {
